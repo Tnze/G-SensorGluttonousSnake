0f678d227873a7ef048d3c90cf5eb0316720bfce
JunDao <Tnze@Tnze.local>
2018-11-03 13:46:28 +0800

    第一波优化

--- gluSnake8x8.c
+++ gluSnake8x8.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
-#define Cell struct cell
 
 //LED状态
 char leds[8];
 
-//贪吃蛇的一格
-Cell
-{
-	char x; //此格的x坐标
-	char y; //此格的y坐标
-};
-
-Cell snake[8 * 8];
+char snake[8 * 8]; //每格前4bit为x坐标，后4bit为y坐标
 int snake_len = 1; //贪吃蛇长度
 
 //输出现在的状态
@@ -33,30 +25,26 @@ void reset()
 	//TODO 重置游戏
 }
 
-char food[2]; //食物位置，[0]x，[1]y
-char dir;	 //行进方向，00上，01下，10右，11左
+char food;	//食物位置
+char dir = 3; //行进方向，0b00上，0b01下，0b10右，0b11左
 
 //计算下一个位置
-void nextCell(char *x, char *y)
+void nextCell(char *next)
 {
-	Cell head = snake[snake_len - 1];
+	char head = snake[snake_len - 1];
 	switch (dir) //计算下一步的位置
 	{
 	case 0b00: //上
-		*x = head.x;
-		*y = head.y - 1;
+		*next = head - 1;
 		break;
 	case 0b01: //下
-		*x = head.x;
-		*y = head.y + 1;
+		*next = head + 1;
 		break;
 	case 0b10: //右
-		*x = head.x + 1;
-		*y = head.y;
+		*next = head + 0b10000;
 		break;
 	case 0b11: //左
-		*x = head.x - 1;
-		*y = head.y;
+		*next = head - 0b10000;
 		break;
 	}
 }
@@ -77,51 +65,48 @@ void moveSnake()
 //游戏刻
 void step()
 {
-	char next_x, next_y;
-	nextCell(&next_x, &next_y);
+	char next;
+	nextCell(&next);
 
 	//检查是否咬到自己
 	for (int i = 0; i < snake_len; i++)
-		if (snake[i].x == next_x && snake[i].y == next_y) //咬到自己了
+		if (snake[i] == next) //咬到自己了
 			reset();
 
 	//检查是否吃到食物
-	if (food[0] == next_x && food[1] == next_y)
+	if (food == next)
 	{ //吃到食物
 		setFood();
 		moveSnake();
-		snake[0].x = next_x;
-		snake[0].y = next_y;
+		snake[0] = next;
 		snake_len++;
 	}
 	else
 	{ //没有吃到食物
 		moveSnake();
-		snake[0].x = next_x;
-		snake[0].y = next_y;
+		snake[0] = next;
 	}
 }
 
+//把数组蛇映射到LED状态
 void snake_to_LED()
 {
-	for (int i = 0; i < 8; i++)
+	for (int i = 0; i < 8; i++) //先熄灭所有灯
 		leds[i] = 0;
-	for (int i = 0; i < snake_len; i++)
-		leds[snake[i].y] |= 0b10000000 >> snake[i].x;
+	for (int i = 0; i < snake_len; i++) //有蛇的位置点亮
+		leds[snake[i] & 0b1111] |= 0b10000000 >> (snake[i] >> 4);
 }
 
 int main(void)
 {
-	printLEDs();
 	setFood();
-	snake[0].x = 3;
-	snake[0].y = 3;
+	snake[0] = (3 << 4) + 5;
 	while (1)
 	{
-		step();
 		snake_to_LED();
 		printLEDs();
 		sleep(1);
+		step();
 	}
 	return 0;
 }
